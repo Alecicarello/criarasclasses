babc1050b6defcc5b14a26d587b8f811f373b69d
Alecicarello <144715168+Alecicarello@users.noreply.github.com>
2023-09-15 17:16:11 -0300

    crias as classes teste

---
+++ app/src/main/java/devandroid/alessandra/criarasclasses/MainActivity.java
@@ -0,0 +1,14 @@
+package devandroid.alessandra.criarasclasses;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
