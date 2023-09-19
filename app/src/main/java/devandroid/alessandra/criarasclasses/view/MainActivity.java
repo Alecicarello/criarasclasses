b0d357b06c26539cba6e8edf6d2589ed4036e75e
Alecicarello <144715168+Alecicarello@users.noreply.github.com>
2023-09-19 16:25:45 -0300

    SETTS E GETTS

--- app/src/main/java/devandroid/alessandra/criarasclasses/view/MainActivity.java
+++ app/src/main/java/devandroid/alessandra/criarasclasses/view/MainActivity.java
@@ -9,14 +9,23 @@ import devandroid.alessandra.criarasclasses.model.Pessoa;
 
 
 public class MainActivity extends AppCompatActivity {
-  Pessoa pessoa;
+
+        Pessoa pessoa;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+
         pessoa = new Pessoa();
+        //Atribuir conteúdo, dados, vqalores para o Objeto.
+        // Conforme o seu Modelo, TEMPLETE
+        pessoa.setPrimeiroNome("Alessandra");
+        pessoa.setSobreNome("Cicarello");
+        pessoa.setCursoDesejado("Android");
+        pessoa.setTelefoneContato("31-991826799");
 
 
     }
