9d75579cb7318e56d738d1408552c182b2876268
Alecicarello <144715168+Alecicarello@users.noreply.github.com>
2023-09-19 16:25:45 -0300

    SETTS OK

--- app/src/main/java/devandroid/alessandra/criarasclasses/view/MainActivity.java
+++ app/src/main/java/devandroid/alessandra/criarasclasses/view/MainActivity.java
@@ -9,14 +9,54 @@ import devandroid.alessandra.criarasclasses.model.Pessoa;
 
 
 public class MainActivity extends AppCompatActivity {
-  Pessoa pessoa;
+
+        Pessoa pessoa;
+        Pessoa outraPessoa;
+
+        String dadosPessoa;
+        String dadosOutraPessoa;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        pessoa = new Pessoa();
+
+         pessoa = new Pessoa();
+        //Atribuir conteúdo, dados, vqalores para o Objeto.
+        // Conforme o seu Modelo, TEMPLATE
+        pessoa.setPrimeiroNome("Alessandra");
+        pessoa.setSobreNome("Cicarello");
+        pessoa.setCursoDesejado("Android");
+        pessoa.setTelefoneContato("31-991826799");
+
+        outraPessoa=new Pessoa();
+        outraPessoa.setPrimeiroNome("Laura");
+        outraPessoa.setSobreNome("Cicarello");
+        outraPessoa.setCursoDesejado("Java");
+        outraPessoa.setTelefoneContato("31-99999988");
+
+        dadosPessoa = "Primeiro nome: ";
+        dadosPessoa += pessoa.getPrimeiroNome();
+        dadosPessoa += " Sobrenome: ";
+        dadosPessoa += pessoa.getSobreNome();
+        dadosPessoa += " Curso Desejado: ";
+        dadosPessoa += pessoa.getCursoDesejado();
+        dadosPessoa += " Telefone de Contato: ";
+        dadosPessoa += pessoa.getTelefoneContato();
+
+
+        dadosOutraPessoa = "Primeiro nome: ";
+        dadosOutraPessoa += outraPessoa.getPrimeiroNome();
+        dadosOutraPessoa += " Sobrenome: ";
+        dadosOutraPessoa += outraPessoa.getSobreNome();
+        dadosOutraPessoa += " Curso Desejado: ";
+        dadosOutraPessoa += outraPessoa.getCursoDesejado();
+        dadosOutraPessoa += " Telefone de Contato: ";
+        dadosOutraPessoa += outraPessoa.getTelefoneContato();
+
+        int parada = 0;
 
 
     }
